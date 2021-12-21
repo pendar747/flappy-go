174f6f06f3faef3ec212a5e3e9b34650909c4334
Pendar Ghalamchi <pendar747@gmail.com>
2021-12-21 22:53:51 +0000

    basic stuff

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/pendar747/flappy-gopher
+
+go 1.16
+
+require (
+	github.com/veandco/go-sdl2 v0.4.10 // indirect
+	golang.org/x/tools v0.1.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	img "github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+	ttf "github.com/veandco/go-sdl2/ttf"
+)
+
+func keepAlive(duration *time.Duration) {
+	start := time.Now()
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			if duration != nil && time.Since(start) > *duration {
+				println("Time up")
+				running = false
+				break
+			}
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
+}
+
+func run() error {
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		return fmt.Errorf("could not initialize SDL: %v", err)
+	}
+	defer sdl.Quit()
+
+	if err := ttf.Init(); err != nil {
+		return fmt.Errorf("could not initialize TTF: %v", err)
+	}
+
+	window, renderer, err := sdl.CreateWindowAndRenderer(800, 600, sdl.WINDOW_SHOWN)
+	// w2, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	// 	800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("could not create window: %v", err)
+	}
+	defer window.Destroy()
+
+	if err := drawTitle(renderer); err != nil {
+		return fmt.Errorf("could not draw title: %v", err)
+	}
+
+	d := 2 * time.Second
+	keepAlive(&d)
+
+	if err := drawBackground(renderer); err != nil {
+		return fmt.Errorf("could not draw background: %v", err)
+	}
+
+	keepAlive(nil)
+
+	return nil
+}
+
+func drawTitle(renderer *sdl.Renderer) error {
+	renderer.Clear()
+
+	font, err := ttf.OpenFont("res/fonts/YujiHentaiganaAkari-Regular.ttf", 20)
+	if err != nil {
+		return fmt.Errorf("could not open font: %v", err)
+	}
+
+	s, err := font.RenderUTF8Solid("Flappy Gopher", sdl.Color{
+		R: 73,
+		G: 229,
+		B: 156,
+	})
+	if err != nil {
+		return fmt.Errorf("could not render title: %v", err)
+	}
+
+	texture, err := renderer.CreateTextureFromSurface(s)
+	if err != nil {
+		return fmt.Errorf("could not create texture: %v", err)
+	}
+	if err := renderer.Copy(texture, nil, nil); err != nil {
+		return fmt.Errorf("could not render texture: %v", err)
+	}
+
+	renderer.Present()
+
+	return nil
+}
+
+func drawBackground(r *sdl.Renderer) error {
+	r.Clear()
+
+	t, err := img.LoadTexture(r, "res/jpg/Mountain1.jpg")
+	if err != nil {
+		return fmt.Errorf("could not load image: %v", err)
+	}
+
+	if err := r.Copy(t, nil, nil); err != nil {
+		return fmt.Errorf("could not draw background")
+	}
+
+	r.Present()
+	return nil
+}
+
+func main() {
+	err := run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error in run: %v", err)
+		os.Exit(2)
+	}
+}
